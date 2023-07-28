0cdcddca3260adc5a514a3a1c1b1d76f5101abcc
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-28 09:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+let rope= document.getElementById("rope-lengths");
 function calculateMinCost() {
+	let val=rope.value.split(",")
+    let arr=[];
+    // console.log(Number(val[0]))
+    let sum=Number(val[0]);
+    // console.log(sum)
+    let sum2=0;
+    for(let i=1;i<val.length;i++){
+        sum+=Number(val[i]);
+        arr.push(sum);
+       
+       
+    }
+    for(let i=0;i<arr.length;i++){
+        sum2+=arr[i];
+    }
+// console.log(sum2);
+let result=document.getElementById("result");
+result.innerText=sum2;
   //your code here
   
   
